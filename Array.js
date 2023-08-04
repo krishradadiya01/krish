758866eb75f9df8b2e5ccb83ca94c5733e5c6622
Krish Radadiya <krishradadiya666@gmail.com>
2023-08-04 12:04:34 +0530

    krish.js

---
+++ Array.js
@@ -0,0 +1,52 @@
+let uid = ["krish" , "sanket" , "viren"];
+console.log(uid);
+
+let quick = ["Paresh" , "Rekha" , "Krish"];
+quick[1] = "Sanket";
+console.log(quick);
+
+
+let yut = ["Iyer" , "vip" , "rajel"]
+console.log(yut.toString());
+
+
+let revenue = {House: "85 Lacs" , Rooms: "5bhk" , Oppenheimar: "yip"};
+console.log(revenue.Rooms);
+
+
+let length = ["krish" , "sanket" , "viren"]
+length = length.length;
+console.log(length);
+
+
+let came = ["bihari" , "Madhyapr" , "MongoDB"]
+kams = came[came.length - 2];
+console.log(kams);
+
+
+let mr = ["Bomb" , "Pistol" , "Rifel"];
+mr.push("Last");
+console.log(mr);
+
+const Urdu = [];
+Urdu[0] = "mr indian hacker";
+Urdu[1] = "ronaldhino";
+Urdu[2] = "oppenheimar";
+
+console.log(Urdu[1]);
+
+
+let web = ["krish" , "sanket" , "pop"];
+let type = typeof web;
+console.log(type);
+
+
+let rise = ["kisu"];
+let porsche = Array.isArray(rise);
+console.log(porsche)
+
+
+let move = ["popy" , "ronaldo"];
+let oppenheimar = move instanceof Array;
+console.log(oppenheimar);
+
